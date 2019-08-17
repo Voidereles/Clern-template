30dc71313902afca8a75caf3f39776273e3bf6ff
Maciej <maciej.zelazo@protonmail.com>
2019-08-17 17:46:57 +0200

    hover on offer items

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ $('.owl-one').owlCarousel({
     items: 1
 })
 
+$('.owl-two').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 3,
+            nav: false
+        },
+        992: {
+            items: 5,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
 $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
         $('#header').addClass('header-scrolled');
